f8deb1fadfb33b183f035b8d3ea4ee70169e55a4
Firda Riska Pratiwi <firdariskap@gmail.com>
2020-04-29 13:25:02 +0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PRG2_20192_P1_3_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan jumlah anak ayam : ");
+            int N = Convert.ToInt16(Console.ReadLine());
+
+            for(int i = N; i > 0; i--)
+            {
+                if (i>1)
+                {
+                    Console.WriteLine("Anak ayam turun {0}, mati satu tinggal {1}", i, i - 1);
+                }
+                else
+                {
+                    Console.WriteLine("Anak ayam turun {0}, mati satu tinggal induknya", i);
+                }
+            }
+        }
+    }
+}
